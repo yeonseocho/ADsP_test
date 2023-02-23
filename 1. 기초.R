57031b727ee098aa8a1e4612aaa912d9cf233068
yeonseocho <103731501+yeonseocho@users.noreply.github.com>
2023-02-23 14:31:52 +0900

    20220223

---
+++ 1. 기초.R
@@ -0,0 +1,8 @@
+rep(1 ,time=5)
+rep(1:4, each=2), 
+rep(c, each=2)
+A <-paste("a", "b", "c", sep="-")
+paste(A, c("e", "f"))
+paste(A,10,sep="")
+substr("Bigdataanalysis",1,4)
+lm(log(brain)~log(body), data=Animals)
